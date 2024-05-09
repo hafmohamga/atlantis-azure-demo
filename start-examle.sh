89d338197e48a69ce8f77e34ca0242b83c361c5e
HM <95107265+hafmohamga@users.noreply.github.com>
2024-05-09 04:29:37 -0400

    Update start-examle.sh

--- start-examle.sh
+++ start-examle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-URL="https://3ae7-92-119-18-153.ngrok-free.app"
+URL="https://be99-92-119-18-153.ngrok-free.app"
 SECRET="7585644023"
 TOKEN="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"
 USERNAME="hafmohamga"

d2af3793ae46801686d4e5d941a77c766bfd27d6
HM <95107265+hafmohamga@users.noreply.github.com>
2024-05-09 08:15:26 +0000

    change instance name

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 # }
 
 # resource "azurerm_linux_virtual_machine" "example" {
-#   name                = "myVM"
+#   name                = "atlantistestvm"
 #   location            = azurerm_resource_group.example.location
 #   resource_group_name = azurerm_resource_group.example.name
 #   size                = "Standard_DS1_v2"

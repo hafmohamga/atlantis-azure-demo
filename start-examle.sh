567479fb02bbcff1c105b2ceb0be03cfec62363c
HM <95107265+hafmohamga@users.noreply.github.com>
2024-05-09 03:52:10 -0400

    Update start-examle.sh url updated

--- start-examle.sh
+++ start-examle.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-URL=" https://a7ef-92-119-18-153.ngrok-free.app"
+URL="https://3ae7-92-119-18-153.ngrok-free.app"
 SECRET="7585644023"
 TOKEN="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"
 USERNAME="hafmohamga"
-REPO_ALLOWLIST="https://github.com/hafmohamga/atlantis-azure-demo" # Replace this with your Azure DevOps repository allowlist
+REPO_ALLOWLIST="github.com/hafmohamga/atlantis-azure-demo" # Replace this with your Azure DevOps repository allowlist
 
 # Any environment variables required for auth
 # Set up Azure DevOps credentials or other necessary environment variables 
